710c7b934560ff03082e0fed4506b6a73427ff45
kokoyroy <kokoyroy@gmail.com>
2021-02-08 17:26:18 +0200

    mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,5 @@
-console.log('test!');
+const overlay = document.querySelector('#overlay')
+const menu = document.querySelector('#menu')
+menu.addEventListener('click', () => {
+    overlay.classList.toggle('active')
+})
